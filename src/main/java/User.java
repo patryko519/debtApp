a29ce7eeb4f4e70a38fe650ed4c53ed0d47249cf
Patryk <patryko51952@gmail.com>
2022-07-21 22:32:48 +0200

    adding user from user class + setters and getters

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,9 +1,40 @@
-public class User {
-    private String user_name;
+import java.sql.SQLException;
+import java.util.Scanner;
 
-    public User(String user_name){
-        this.user_name = user_name;
+public class User {
+    private String userName;
+
+    public static boolean isAlpha(String s) {
+        return s != null && s.matches("^[a-zA-Z]*$");
     }
 
+    public User(){
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void addUserToDatabase(){
+        System.out.println("Enter name of new user");
+        Scanner scanner = new Scanner(System.in);
+
+        String user_name = scanner.nextLine();
+        while (!isAlpha(user_name)){
+            user_name = scanner.nextLine();
+        }
+
+        setUserName(user_name);
+        try {
+            DatabaseConnection.addUser(user_name);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("User " + user_name + " has been added to database");
+    }
 
 }
