847bae54a38d51d86f27cd31533a46c7fc7fc316
Patryk <patryko51952@gmail.com>
2022-07-21 21:20:17 +0200

    New class User with constructor and Main file created

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean shouldContinue = true;
+        int userChoice;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome. Choose your option");
+
+        while(shouldContinue){
+            System.out.println("1. Add user to database");
+            System.out.println("2. Add new transaction");
+            System.out.println("3. Check your transactions");
+
+        }
+    }
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,22 +1,9 @@
-//import java.beans.Statement;
-//
-//public class User {
-//    private String user_name;
-//
-//
-//    public String getUser_name() {
-//        return user_name;
-//    }
-//
-//    public void setUser_name(String user_name) {
-//        this.user_name = user_name;
-//    }
-//
-//    public void addUserToDatabase(){
-//        DatabaseConnection.connection();
-//
-//        String query = "INSERT INTO users(user_name) VALUES(" + user_name + ");";
-//        //ta zmienna jest w DatabaseConnection.connection();
-//        Statement statement = connection.createStatement();
-//    }
-//}
+public class User {
+    private String user_name;
+
+    public User(String user_name){
+        this.user_name = user_name;
+    }
+
+
+}
