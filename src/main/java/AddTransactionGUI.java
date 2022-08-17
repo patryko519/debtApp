6a57dc718ec3f3daaf8f03379d8e0ffa6587cb79
Patryk <patryko51952@gmail.com>
2022-08-17 23:38:50 +0200

    addtransactionGUI done in 50%

--- src/main/java/AddTransactionGUI.java
+++ src/main/java/AddTransactionGUI.java
@@ -3,6 +3,10 @@ import java.sql.SQLException;
 import java.util.Vector;
 
 public class AddTransactionGUI extends MainFrame{
+
+    private JComboBox<String> list;
+    private SpinnerModel model;
+    private JSpinner spinner;
     public AddTransactionGUI(int userId, String username) throws SQLException {
 
         frame.add(panel);
@@ -14,24 +18,44 @@ public class AddTransactionGUI extends MainFrame{
         userLabel.setBounds(10,20,80,25);
         panel.add(userLabel);
 
-        JComboBox<String> list = new JComboBox<>(names);
+        list = new JComboBox<>(names);
         list.setBounds(100,20,165,25);
         panel.add(list);
-
-//        userText = new JTextField(20);
-//        userText.setBounds(100,20,165,25);
-//        panel.add(userText);
 
         JLabel amountLabel = new JLabel("Amount");
         amountLabel.setBounds(10,50,80,25);
         panel.add(amountLabel);
 
-//        passwordText = new JPasswordField();
-//        passwordText.setBounds(100, 50, 165, 25);
-//        panel.add(passwordText);
+        //better number input
+        model = new SpinnerNumberModel(9.9, 0.00, 10000, 0.1);
+        spinner = new JSpinner(model);
+        spinner.setBounds(100,50,80,25);
+        panel.add(spinner);
 
         JLabel descriptionLabel = new JLabel("Description");
         descriptionLabel.setBounds(10,80,80,25);
         panel.add(descriptionLabel);
+
+
+        //need to change height
+        JTextField transactionDescription = new JTextField();
+        transactionDescription.setBounds(100,80,165,50);
+        panel.add(transactionDescription);
+
+        JButton addTransactionButton = new JButton("Add");
+        addTransactionButton.setBounds(5,110,200,25);
+        panel.add(addTransactionButton);
+        addTransactionButton.addActionListener(e -> addTransaction());
+
+        JButton backButton = new JButton("Log out");
+        backButton.setBounds(5,120,200,25);
+        panel.add(backButton);
+        backButton.addActionListener(e -> new ManagementSystemGUI(userId,username));
+        backButton.addActionListener(e -> frame.dispose());
+    }
+
+    public void addTransaction(){
+        //String selectedFruit = "You selected " + list.getItemAt(list.getSelectedIndex());
+        System.out.println(list.getItemAt(list.getSelectedIndex()));
     }
 }
