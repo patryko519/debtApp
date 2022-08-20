208257acfd7ee273c169eefb02a113389cc8b34d
Patryk <patryko51952@gmail.com>
2022-08-20 17:40:32 +0200

    incoming/outgoing done, changed layout, double positive numeric amount

--- src/main/java/AddTransactionGUI.java
+++ src/main/java/AddTransactionGUI.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.sql.SQLException;
 import java.util.Vector;
 
 public class AddTransactionGUI extends Frame {
 
     private JComboBox<String> list;
-    private SpinnerModel model;
-    private JSpinner spinner;
+    private JTextField amountField;
     private JToggleButton test;
     private int typeOfTransaction;
     private String username;
@@ -23,34 +24,54 @@ public class AddTransactionGUI extends Frame {
         Vector<String> names = DatabaseConnection.usersNames();
         names.remove(username);
 
+        test = new JToggleButton("Incoming");
+        test.setBounds(192,10,100,25);
+        panel.add(test);
+        test.addActionListener(e -> typeOfTransaction());
+
         JLabel userLabel = new JLabel("User");
-        userLabel.setBounds(10,20,80,25);
+        userLabel.setBounds(10,50,80,25);
         panel.add(userLabel);
 
         list = new JComboBox<>(names);
-        list.setBounds(100,20,165,25);
+        list.setBounds(100,50,165,25);
         panel.add(list);
 
         JLabel amountLabel = new JLabel("Amount");
-        amountLabel.setBounds(10,50,80,25);
+        amountLabel.setBounds(10,80,80,25);
         panel.add(amountLabel);
 
-        //better number input
-        model = new SpinnerNumberModel(9.9, 0.00, 10000, 0.1);
-        spinner = new JSpinner(model);
-        spinner.setBounds(100,50,80,25);
-        panel.add(spinner);
+        amountField = new JTextField("0.00");
+        amountField.setBounds(100,80,80,25);
+        panel.add(amountField);
+
+        amountField.addKeyListener(new KeyAdapter() {
+            int a = 0;
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+
+                if(c == '.'){
+                    if(a>0){
+                        e.consume();
+                    }
+                    a++;
+                } else if(!Character.isDigit(c)){
+                    e.consume();
+                }
+            }
+        });
 
         JLabel descriptionLabel = new JLabel("Description");
-        descriptionLabel.setBounds(10,80,80,25);
+        descriptionLabel.setBounds(10,110,80,25);
         panel.add(descriptionLabel);
 
         transactionDescription = new JTextField();
-        transactionDescription.setBounds(100,80,165,50);
+        transactionDescription.setBounds(100,110,165,50);
         panel.add(transactionDescription);
 
         JButton addTransactionButton = new JButton("Add");
-        addTransactionButton.setBounds(5,140,200,25);
+        addTransactionButton.setBounds(5,170,200,25);
         panel.add(addTransactionButton);
         addTransactionButton.addActionListener(e -> {
             try {
@@ -60,20 +81,21 @@ public class AddTransactionGUI extends Frame {
             }
         });
 
-        JButton backButton = new JButton("Log out");
-        backButton.setBounds(5,170,200,25);
+        JButton backButton = new JButton("Back");
+        backButton.setBounds(5,200,200,25);
         panel.add(backButton);
         backButton.addActionListener(e -> new ManagementSystemGUI(userId,username));
         backButton.addActionListener(e -> frame.dispose());
-
-        test = new JToggleButton("Incoming");
-        test.setBounds(5,200,100,25);
-        panel.add(test);
-        test.addActionListener(e -> typeOfTransaction());
     }
 
     public void addTransaction() throws SQLException {
-        DatabaseConnection.addTransaction(username, list.getItemAt(list.getSelectedIndex()), (double) spinner.getValue(), transactionDescription.getText());
+        if(typeOfTransaction == 1){
+            DatabaseConnection.addTransaction(username, list.getItemAt(list.getSelectedIndex()), Double.parseDouble(amountField.getText()), transactionDescription.getText());
+        } else{
+            DatabaseConnection.addTransaction( list.getItemAt(list.getSelectedIndex()),username, Double.parseDouble(amountField.getText()), transactionDescription.getText());
+        }
+
+        JOptionPane.showMessageDialog(this,"Transaction has been added");
     }
 
     public void typeOfTransaction(){
