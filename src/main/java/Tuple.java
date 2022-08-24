5fc100babb81cebc08f49a8d4aff105ad91972d1
Patryk <patryko51952@gmail.com>
2022-08-24 15:56:50 +0200

    tuple class, use in returning transaction details in vector

---
+++ src/main/java/Tuple.java
@@ -0,0 +1,9 @@
+public class Tuple<Double, String> {
+    public Double amount;
+    public String description;
+
+    public Tuple(Double amount, String description) {
+        this.amount = amount;
+        this.description = description;
+    }
+}
